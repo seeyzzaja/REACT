29303cae0deea12b3a2269b6f92fcedff25b1603
seeyzzaja <seeyzzaja@gmail.com>
2026-01-01 15:24:09 +0700

    add_ulangan_pekan3

--- PEKAN4/day6_project/src/components/PrivateRoute.tsx
+++ PEKAN4/day6_project/src/components/PrivateRoute.tsx
@@ -1,7 +1,7 @@
 import { Navigate } from 'react-router-dom'
 import { useAuth } from '../hooks/useAuth'
 
-export default function PrivateRoute({ children }: { children: JSX.Element }) {
+export default function PrivateRoute({ children }: { children: React.ReactNode }) {
   const { isAuth } = useAuth()
   return isAuth ? children : <Navigate to="/login" replace />
 }
